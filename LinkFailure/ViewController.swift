8bc41fd1ea303bc13dab4d274231b4c97d09f87f
Barry P. Medoff <barry@medoff.com>
2020-08-31 11:31:05 -0700

    Initial Commit

---
+++ LinkFailure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkFailure
+//
+//  Created by Barry P. Medoff on 8/31/20.
+//  Copyright © 2020 Transition Technology Ventures, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
